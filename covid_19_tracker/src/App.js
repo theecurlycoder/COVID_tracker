2eda28cedc609826d84dddbcc29c4558314bbdc7
Megan-Kim <theecurlycoder@gmail.com>
2020-09-16 17:58:11 +0200

    Div for header and dropdown added

--- covid_19_tracker/src/App.js
+++ covid_19_tracker/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="app">
+      <div className="app__header">
       <h1> COVID 19 Tracker App</h1>
+      <FormControl className = "app__dropdown">
+        <Select
+          variant="outlined"
+          value="abc"
+        >
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Option 2</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+          <MenuItem value="worldwide">Hello World</MenuItem>
+          <MenuItem value="worldwide">Yooooooo</MenuItem>
+          
 
+        </Select>
+      </FormControl> 
+      </div>
+     
       {/* todo: header*/}
       {/* todo:Title + Select input dropdown field */}
 
@@ -16,7 +36,7 @@ function App() {
       {/* todo: info box 3 */}
 
       {/* todo: Table*/}
-      
+
       {/* todo: Graph*/}
 
       {/* todo: Map */}
