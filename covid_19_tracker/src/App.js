f07599fa26379c4cbde9d218bc2472ce5fe539c2
Megan-Kim <theecurlycoder@gmail.com>
2020-09-16 17:01:06 +0200

    added todo markers for each section of the app

--- covid_19_tracker/src/App.js
+++ covid_19_tracker/src/App.js
@@ -6,6 +6,20 @@ function App() {
   return (
     <div className="app">
       <h1> COVID 19 Tracker App</h1>
+
+      {/* todo: header*/}
+      {/* todo:Title + Select input dropdown field */}
+
+
+      {/* todo: info box 1 */}
+      {/* todo: info box 2 */}
+      {/* todo: info box 3 */}
+
+      {/* todo: Table*/}
+      
+      {/* todo: Graph*/}
+
+      {/* todo: Map */}
     </div>
   );
 }
