722ecb292431e5a40602b3eadd1722b4edc6f16b
Megan-Kim <mk.chinian@gmail.com>
2020-09-16 16:44:38 +0200

    React App Created
    - Created npx app 
    - Project cleanup

---
+++ covid_19_tracker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1> COVID 19 Tracker App</h1>
+    </div>
+  );
+}
+
+export default App;
